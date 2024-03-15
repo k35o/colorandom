6d25f6eb24853c03e8b9645e1cd4a75366cb65b8
Koki Sakano <kosakanoki@gmail.com>
2024-03-15 19:19:53 +0900

    write code

---
+++ mod.ts
@@ -0,0 +1,14 @@
+/**
+ * A module providing a function to generate a random color by hexadecimal string.
+ *
+ * @example
+ * ```ts
+ * import { colorandom } from "@k8o/colorandom";
+ *
+ * colorandom();
+ * ```
+ *
+ * @module
+ */
+
+export * from "./main.ts";
