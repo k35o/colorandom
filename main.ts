0d2242de25fdc0e9c1601d5f43c25e2611a854b6
Koki Sakano <kosakanoki@gmail.com>
2024-03-17 10:26:57 +0900

    いろいろな色の形式をサポートする

--- main.ts
+++ main.ts
@@ -1,4 +1,23 @@
-/** This function gets color code by Math.random. */
-export function colorandom(): string {
+type Format = "hex" | "rgb" | "hsl";
+
+/**
+ * This function gets color code by Math.random.
+ *
+ * @param {Format} type - The format of the color code.
+ * @returns {string} The color code.
+ */
+export function colorandom(
+  type: Format = "hex",
+): string {
+  if (type === "rgb") {
+    return `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${
+      Math.random() * 255
+    })`;
+  }
+  if (type === "hsl") {
+    return `hsl(${Math.random() * 360}, ${Math.random() * 100}%, ${
+      Math.random() * 100
+    }%)`;
+  }
   return "#" + Math.random().toString(16).slice(2, 8);
 }
--- main_test.ts
+++ main_test.ts
@@ -1,6 +1,41 @@
 import { assertEquals } from "https://deno.land/std@0.220.0/assert/mod.ts";
 import { colorandom } from "./main.ts";
 
-Deno.test(function addTest() {
-  assertEquals(colorandom().length, 7);
-});
+Deno.test(
+  "colorandom() returns a random color code by default",
+  () => {
+    const actual = colorandom();
+    assertEquals(typeof actual, "string");
+    assertEquals(actual.length, 7);
+  },
+);
+
+Deno.test(
+  "colorandom('hex') returns a random color code by hexadecimal string",
+  () => {
+    const actual = colorandom("hex");
+    assertEquals(typeof actual, "string");
+    assertEquals(actual.length, 7);
+    assertEquals(actual.startsWith("#"), true);
+  },
+);
+
+Deno.test(
+  "colorandom('rgb') returns a random color code by RGB string",
+  () => {
+    const actual = colorandom("rgb");
+    assertEquals(typeof actual, "string");
+    assertEquals(actual.startsWith("rgb("), true);
+    assertEquals(actual.endsWith(")"), true);
+  },
+);
+
+Deno.test(
+  "colorandom('hsl') returns a random color code by HSL string",
+  () => {
+    const actual = colorandom("hsl");
+    assertEquals(typeof actual, "string");
+    assertEquals(actual.startsWith("hsl("), true);
+    assertEquals(actual.endsWith(")"), true);
+  },
+);
